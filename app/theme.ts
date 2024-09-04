2de88c425c98cdb94d9d047fc71e475ebe864109
Oscar Noe Ortiz Barba <oortizbarba@gmail.com>
2024-09-03 23:23:05 -0600

    first commit

---
+++ app/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#264772',
+    },
+    secondary: {
+      main: '#122946',
+    },
+  },
+});
+
+export default theme;
