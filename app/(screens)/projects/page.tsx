b94356fb95d79c69d9521a27a0b8aac6243994e0
Oscar Noe Ortiz Barba <oortizbarba@gmail.com>
2024-09-24 23:13:24 -0600

    Added grid layout

--- app/(screens)/projects/page.tsx
+++ app/(screens)/projects/page.tsx
@@ -1,26 +1,36 @@
 'use client';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Grid2, Typography } from '@mui/material';
 import React from 'react';
-import theme from '@/app/theme/theme';
 
 const ProjectsPage = () => {
-
   return (
-    <Box sx={{ marginBottom: 2 }}>
-      <Typography>
-        Nos complace ofrecerle una amplia gama de
-        propiedades exclusivas y servicios
-        personalizados para satisfacer todas sus
-        necesidades inmobiliarias. Nuestra misión es
-        ayudarle a encontrar la casa de sus sueños o la
-        inversión perfecta, con el respaldo de nuestro
-        equipo de expertos comprometidos con la
-        excelencia y la transparencia. Descubra cómo
-        podemos hacer realidad sus aspiraciones
-        inmobiliarias.
-      </Typography>
-    </Box>
+    <Grid2 container spacing={2} sx={{ paddingInline: 5, paddingBlock: 3 }}>
+      {/* Fila que contiene las dos columnas */}
+      <Grid2 container size={{ xs: 12 }} spacing={2}>
+        {/* Primera columna */}
+        <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
+          <Typography fontFamily={'inherit'}>
+            Nos complace ofrecerle una amplia gama de propiedades exclusivas y servicios
+            personalizados para satisfacer todas sus necesidades inmobiliarias. Nuestra misión
+            es ayudarle a encontrar la casa de sus sueños o la inversión perfecta, con el respaldo
+            de nuestro equipo de expertos comprometidos con la excelencia y la transparencia.
+            Descubra cómo podemos hacer realidad sus aspiraciones inmobiliarias.
+          </Typography>
+        </Grid2>
+
+        {/* Segunda columna */}
+        <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
+          <Typography fontFamily={'inherit'}>
+            Nos complace ofrecerle una amplia gama de propiedades exclusivas y servicios
+            personalizados para satisfacer todas sus necesidades inmobiliarias. Nuestra misión
+            es ayudarle a encontrar la casa de sus sueños o la inversión perfecta, con el respaldo
+            de nuestro equipo de expertos comprometidos con la excelencia y la transparencia.
+            Descubra cómo podemos hacer realidad sus aspiraciones inmobiliarias.
+          </Typography>
+        </Grid2>
+      </Grid2>
+    </Grid2>
   );
 };
 
