ea3a76ef6adffe1523455f006444ed09fc31817b
OscarCALAGroup <oortizbarba@gmail.com>
2025-02-11 16:29:23 -0600

    Added completed projects images

--- app/(screens)/projects/completed/page.tsx
+++ app/(screens)/projects/completed/page.tsx
@@ -4,6 +4,15 @@ import { CustomSubTitle } from '@/app/components/Titles/CustomSubTitle/CustomSub
 import { CustomTitle } from '@/app/components/Titles/CustomTitle/CustomTitle'
 import { Grid2, Typography } from '@mui/material'
 import React from 'react'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CustomEmblaCarousel from '@/app/components/Carousel/CustomCarusel';
+import { itemsLomasDelOcote } from './photos/FraccionamientoLocasDelOcote';
+import { itemsCotoBamboo } from './photos/CotoBamboo';
+import { itemsCabaniasLupita1 } from './photos/CabaniasLupita1';
+import { itemsCabaniasLupita2 } from './photos/CabaniasLupita2';
+import { itemsCasaDeLujoSantaElena } from './photos/CasaDeLujoSantaElena';
+import { itemsFraccionamientoSantaElena } from './photos/FraccionamientoSantaElena';
+import { itemsOficinasCS } from './photos/OficinasCS';
 
 const ProjectsCompleted = () => {
   return (
@@ -40,21 +49,44 @@ const ProjectsCompleted = () => {
         <Grid2 size={{ xs: 12, sm: 12, md: 12 }} paddingBlock={1}>
           <AnimatedText direction="right">
             <Typography variant='h4' fontFamily={'Bebas Neue'}>
-              {"Fraccionamiento Lomas del Ocote, Ixtlahuacán Del Río, 2020"}
+              {"Fraccionamiento Lomas del Ocote"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Ixtlahuacán Del Río"}
+              <LocationOnIcon fontSize='small' />
+              {"2020"}
             </Typography>
           </AnimatedText>
         </Grid2>
-        {/* <CustomEmblaCarousel photos={itemsLosFresnos} /> */}
+        <CustomEmblaCarousel photos={itemsLomasDelOcote} />
       </Grid2>
 
       <Grid2 container size={{ xs: 12 }} spacing={2} sx={{ paddingInline: 5, paddingBlock: 3 }}>
         <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
           <AnimatedText direction="right">
             <Typography variant='h4' fontFamily={'Bebas Neue'}>
-              {"Coto Bamboo, Guadalajara, 2023"}
+              {"Coto Bamboo"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Guadalajara"}
+              <LocationOnIcon fontSize='small' />
+              {"2023"}
             </Typography>
           </AnimatedText>
         </Grid2>
+        <CustomEmblaCarousel photos={itemsCotoBamboo} />
       </Grid2>
 
       <Grid2
@@ -70,20 +102,45 @@ const ProjectsCompleted = () => {
         <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
           <AnimatedText direction="right">
             <Typography variant='h4' fontFamily={'Bebas Neue'}>
-              {"Cabañas Lupita, Mazamitla, 2024"}
+              {"Cabañas Lupita"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Mazamitla"}
+              <LocationOnIcon fontSize='small' />
+              {"2024"}
             </Typography>
           </AnimatedText>
         </Grid2>
+        <CustomEmblaCarousel photos={itemsCabaniasLupita1} />
+        <CustomEmblaCarousel photos={itemsCabaniasLupita2} />
       </Grid2>
 
       <Grid2 container size={{ xs: 12 }} spacing={2} sx={{ paddingInline: 5, paddingBlock: 3 }}>
         <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
           <AnimatedText direction="right">
             <Typography variant='h4' fontFamily={'Bebas Neue'}>
-              {"Casa de lujo Santa Elena, 2024"}
+              {"Casa de lujo Santa Elena"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Mazamitla"}
+              <LocationOnIcon fontSize='small' />
+              {"2024"}
             </Typography>
           </AnimatedText>
         </Grid2>
+        <CustomEmblaCarousel photos={itemsCasaDeLujoSantaElena} />
       </Grid2>
 
       <Grid2
@@ -99,20 +156,53 @@ const ProjectsCompleted = () => {
         <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
           <AnimatedText direction="right">
             <Typography variant='h4' fontFamily={'Bebas Neue'}>
-              {"Fraccionamiento Santa Elena, Mazamitla, 2024"}
+              {"Fraccionamiento Santa Elena"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Mazamitla"}
+              <LocationOnIcon fontSize='small' />
+              {"2024"}
             </Typography>
           </AnimatedText>
         </Grid2>
+        <CustomEmblaCarousel photos={itemsFraccionamientoSantaElena} />
       </Grid2>
 
       <Grid2 container size={{ xs: 12 }} spacing={2} sx={{ paddingInline: 5, paddingBlock: 3 }}>
         <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
           <AnimatedText direction="right">
             <Typography variant='h4' fontFamily={'Bebas Neue'}>
-              {"Oficinas CS Ingenierías, Valle de Juárez, Mazamitla, 2024"}
+              {"Oficinas CS Ingenierías"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Valle de Juárez y Mazamitla"}
+              <LocationOnIcon fontSize='small' />
+              {"2024"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"(Antes y después)"}
             </Typography>
           </AnimatedText>
         </Grid2>
+        <CustomEmblaCarousel photos={itemsOficinasCS} />
       </Grid2>
 
     </Grid2>
---
+++ app/(screens)/projects/completed/photos/FraccionamientoLocasDelOcote.ts
@@ -0,0 +1,32 @@
+export const itemsLomasDelOcote = [
+  {
+    src: '/assets/images/projects/completed/FraccionamientoLomasDelOcote/FLDO1.png',
+    altText: '',
+    caption: '',
+  },
+  {
+    src: '/assets/images/projects/completed/FraccionamientoLomasDelOcote/FLDO2.png',
+    altText: '',
+    caption: '',
+  },
+  {
+    src: '/assets/images/projects/completed/FraccionamientoLomasDelOcote/FLDO3.png',
+    altText: '',
+    caption: '',
+  },
+  {
+    src: '/assets/images/projects/completed/FraccionamientoLomasDelOcote/FLDO4.png',
+    altText: '',
+    caption: '',
+  },
+  {
+    src: '/assets/images/projects/completed/FraccionamientoLomasDelOcote/FLDO5.png',
+    altText: '',
+    caption: '',
+  },
+  {
+    src: '/assets/images/projects/completed/FraccionamientoLomasDelOcote/FLDO6.png',
+    altText: '',
+    caption: '',
+  },
+];
