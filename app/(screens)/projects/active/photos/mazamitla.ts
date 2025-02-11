c0513452c8dad47f1c230a41e4100264e4149b54
OscarCALAGroup <oortizbarba@gmail.com>
2025-02-10 23:19:58 -0600

    Added images completed

--- app/(screens)/projects/active/photos/mazamitla.ts
+++ app/(screens)/projects/active/photos/mazamitla.ts
@@ -1,67 +1,67 @@
 
 export const itemsMazamitla: any[] = [
   {
-    src: '/assets/images/projects/mazamitla/M1.jpg',
+    src: '/assets/images/projects/active/mazamitla/M1.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M2.jpg',
+    src: '/assets/images/projects/active/mazamitla/M2.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M3.jpg',
+    src: '/assets/images/projects/active/mazamitla/M3.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M4.jpg',
+    src: '/assets/images/projects/active/mazamitla/M4.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M5.jpg',
+    src: '/assets/images/projects/active/mazamitla/M5.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M6.jpg',
+    src: '/assets/images/projects/active/mazamitla/M6.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M7.jpg',
+    src: '/assets/images/projects/active/mazamitla/M7.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M8.jpg',
+    src: '/assets/images/projects/active/mazamitla/M8.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M9.jpg',
+    src: '/assets/images/projects/active/mazamitla/M9.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M10.jpg',
+    src: '/assets/images/projects/active/mazamitla/M10.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M11.jpg',
+    src: '/assets/images/projects/active/mazamitla/M11.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M12.jpg',
+    src: '/assets/images/projects/active/mazamitla/M12.jpg',
     altText: '',
     caption: '',
   },
   {
-    src: '/assets/images/projects/mazamitla/M13.jpg',
+    src: '/assets/images/projects/active/mazamitla/M13.jpg',
     altText: '',
     caption: '',
   },
