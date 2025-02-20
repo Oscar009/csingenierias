76605e73586ed0427d5ac8f55deffbcb3a1bc14f
OscarCALAGroup <oortizbarba@gmail.com>
2025-02-20 09:03:03 -0600

    init banks

--- app/(screens)/projects/completed/page.tsx
+++ app/(screens)/projects/completed/page.tsx
@@ -13,6 +13,7 @@ import { itemsCabaniasLupita2 } from './photos/CabaniasLupita2';
 import { itemsCasaDeLujoSantaElena } from './photos/CasaDeLujoSantaElena';
 import { itemsFraccionamientoSantaElena } from './photos/FraccionamientoSantaElena';
 import { itemsOficinasCS } from './photos/OficinasCS';
+import { itemsCanchaSantaMariaDelOro } from './photos/CanchasSantaMariaDelOro';
 
 const ProjectsCompleted = () => {
   return (
@@ -203,6 +204,58 @@ const ProjectsCompleted = () => {
           </AnimatedText>
         </Grid2>
         <CustomEmblaCarousel photos={itemsOficinasCS} />
+      </Grid2>
+      
+      <Grid2
+        container
+        size={{ xs: 12 }}
+        spacing={2}
+        sx={{
+          backgroundColor: "#f1f1f1",
+          paddingInline: 5,
+          paddingBlock: 3
+        }}
+      >
+        <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
+          <AnimatedText direction="right">
+            <Typography variant='h4' fontFamily={'Bebas Neue'}>
+              {"Cancha multiusos"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Santa María del Oro"}
+              <LocationOnIcon fontSize='small' />
+            </Typography>
+          </AnimatedText>
+        </Grid2>
+        <CustomEmblaCarousel photos={itemsCanchaSantaMariaDelOro} />
+      </Grid2>
+
+      <Grid2 container size={{ xs: 12 }} spacing={2} sx={{ paddingInline: 5, paddingBlock: 3 }}>
+        <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
+          <AnimatedText direction="right">
+            <Typography variant='h4' fontFamily={'Bebas Neue'}>
+              {"Banco"}
+            </Typography>
+          </AnimatedText>
+          <AnimatedText direction="right">
+            <Typography
+              variant='body1'
+              fontFamily={'Bebas Neue'}
+              sx={{ display: 'flex' }}
+            >
+              {"Localización"}
+              <LocationOnIcon fontSize='small' />
+              {"Año"}
+            </Typography>
+          </AnimatedText>
+        </Grid2>
+        {/* <CustomEmblaCarousel photos={itemsOficinasCS} /> */}
       </Grid2>
 
     </Grid2>
